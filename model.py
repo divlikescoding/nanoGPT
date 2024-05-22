e513da9f996ba13486f9fd294a3d87b6e47cd233
Divyansh Jain <divyanshrajeshjain@gmail.com>
2024-05-22 15:51:08 -0700

    Test

--- model.py
+++ model.py
@@ -76,7 +76,7 @@ class CausalSelfAttention(nn.Module):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k, v  = self.n_kqv_embd(x).split(self.n_embd, dim=2)
+        q, k, v  = self.kqv_proj(x).split(self.n_kqv_embd, dim=2)
         k = k.view(B, T, self.n_head, self.n_kqv_embd).transpose(1, 2) # (B, nh, T, hs)
         q = q.view(B, T, self.n_head, self.n_kqv_embd).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, self.n_kqv_embd).transpose(1, 2) # (B, nh, T, hs)
